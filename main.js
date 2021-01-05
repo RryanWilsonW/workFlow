b57a56a16e59d2791d4b68773c87024748211fac
Ryan Wilson <RryanWilsonW@gmail.com>
2021-01-05 02:38:01 -0500

    Finished javascript file with criteria met.

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 let dayOfWeek = $('#currentDay');
-console.log(dayOfWeek);
 let currentDate = moment().format('LL');
-console.log(currentDate);
 let textBlock = $('.textarea');
-console.log(textBlock);
 dayOfWeek.text(currentDate);
 
 var currentDay = [
-    {hour: "09", time: 09, meridiem: "am"},
-    {hour: "10", time: 10, meridiem: "am"},
-    {hour: "11", time: 11, meridiem: "am"},
-    {hour: "12", time: 12, meridiem: "pm"},
-    {hour: "01", time: 13, meridiem: "pm"},
-    {hour: "02", time: 14, meridiem: "pm"},
-    {hour: "03", time: 15, meridiem: "pm"},
-    {hour: "04", time: 16, meridiem: "pm"},
-    {hour: "05", time: 17, meridiem: "pm"},   
+    {hour: "09", time: 09, meridiem: "am", input: ""},
+    {hour: "10", time: 10, meridiem: "am", input: ""},
+    {hour: "11", time: 11, meridiem: "am", input: ""},
+    {hour: "12", time: 12, meridiem: "pm", input: ""},
+    {hour: "01", time: 13, meridiem: "pm", input: ""},
+    {hour: "02", time: 14, meridiem: "pm", input: ""},
+    {hour: "03", time: 15, meridiem: "pm", input: ""},
+    {hour: "04", time: 16, meridiem: "pm", input: ""},
+    {hour: "05", time: 17, meridiem: "pm", input: ""},   
 ];
 
 currentDay.forEach(function(currentHour) {
@@ -32,15 +29,17 @@ currentDay.forEach(function(currentHour) {
     
     let inputBox = $('<input>')
         inputBox.attr({'class': 'col-md-8 textarea'})
+        inputBox.attr({'id':'inputBox-' + currentHour.time})
         hourRow.append(inputBox)
     
     let submitButton = $('<button>')
         submitButton.attr({'class': 'saveBtn col-md-2'})
+        submitButton.attr({'id':'saveBtn-' + currentHour.time})
         hourRow.append(submitButton)
 
     let buttonImg = $('<img>')
-        buttonImg.attr({'src': '../assets/save-icon.jpg'})
-        buttonImg.attr({'class':'col-md-12'})
+        buttonImg.attr({'src': './assets/save-icon.jpg'})
+        buttonImg.attr({'width': '60%'})
         submitButton.append(buttonImg)
 
     if (currentHour.time > moment().format('HH')) {
@@ -52,5 +51,40 @@ currentDay.forEach(function(currentHour) {
     else {
         inputBox.attr({'class': 'present col-md-8'})
     }
+})
+let nineTodo = localStorage.getItem('9');
+$('#inputBox-9').val(nineTodo);
+
+let tenTodo = localStorage.getItem('10');
+$('#inputBox-10').val(tenTodo);
+
+let elevenTodo = localStorage.getItem('11');
+$('#inputBox-11').val(elevenTodo);
+
+let twelveTodo = localStorage.getItem('12');
+$('#inputBox-12').val(twelveTodo);
+
+let oneTodo = localStorage.getItem('13');
+$('#inputBox-13').val(oneTodo);
+
+let twoTodo = localStorage.getItem('14');
+$('#inputBox-14').val(twoTodo);
+
+let threeTodo = localStorage.getItem('15');
+$('#inputBox-15').val(threeTodo);
+
+let fourTodo = localStorage.getItem('16');
+$('#inputBox-16').val(fourTodo);
+
+let fiveTodo = localStorage.getItem('17');
+$('#inputBox-17').val(fiveTodo);
+
+$('.saveBtn').click(function(){
+    let idFoo = $(this).attr('id')
+    let hour = idFoo.slice(idFoo.indexOf('-') + 1);
+
+let inputFoo = $('#inputBox-' + hour);
+
+localStorage.setItem(hour, inputFoo.val())
 })
 
